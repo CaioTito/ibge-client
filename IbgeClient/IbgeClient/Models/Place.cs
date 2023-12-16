59ce6328a81a0ecab3f0bd1d71005975a23a9404
ezequiel-lima <joao.lima94@fatec.sp.gov.br>
2023-12-16 16:14:54 -0300

    FEAT: Consulta de localidades do Estado de SP

---
+++ IbgeClient/IbgeClient/Models/Place.cs
@@ -0,0 +1,10 @@
+﻿namespace IbgeClient.Models
+{
+    public class Place
+    {
+        public Guid Id { get; set; }
+        public string CodeIbge { get; set; }
+        public string State { get; set; }
+        public string City { get; set; }
+    }
+}
