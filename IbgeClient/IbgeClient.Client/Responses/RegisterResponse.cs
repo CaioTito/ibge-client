6dcc4ae8162577be34bb764702d94c217f6728da
ezequiel-lima <joao.lima94@fatec.sp.gov.br>
2023-12-18 01:13:37 -0300

    FEAT: Registrar novos usuarios

---
+++ IbgeClient/IbgeClient.Client/Responses/RegisterResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace IbgeClient.Client.Responses
+{
+    public class RegisterResponse
+    {
+        public Email email { get; set; }
+        public string password { get; set; }
+    }
+
+    public class Email
+    {
+        public string address { get; set; }
+        public List<object> notifications { get; set; }
+        public bool isValid { get; set; }
+    }
+}
