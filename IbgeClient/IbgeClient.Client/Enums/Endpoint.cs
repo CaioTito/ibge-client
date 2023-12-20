58270119813043c4cf5cb40ffcb41f288c3b4d9c
Christian Souza <chrsouza012@gmail.com>
2023-12-20 12:00:13 -0300

    FEAT: criação de extensão de enum

---
+++ IbgeClient/IbgeClient.Client/Enums/Endpoint.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+
+namespace IbgeClient.Client.Enums
+{
+    public enum Endpoint
+    {
+        #region Autenticação e Autorização
+        [Description("https://localhost:7122/v1/accounts")]
+        Account,
+        [Description("https://localhost:7122/v1/login")]
+        Login,
+        #endregion
+
+        #region Localidades
+        [Description("https://localhost:7122/v1/ibges")]
+        GetIbges,
+        [Description("https://localhost:7122/v1/ibges")]
+        PostIbges,
+        [Description("https://localhost:7122/v1/ibges")]
+        PutIbges,
+        [Description("https://localhost:7122/v1/ibges/city")]
+        GetIbgesCity,
+        [Description("https://localhost:7122/v1/ibges/state")]
+        GetIbgesState,
+        [Description("https://localhost:7122/v1/ibges/codeIbge")]
+        GetCodeIbges,
+        [Description("https://localhost:7122/v1/ibges/id")]
+        DeleteIbges
+        #endregion
+    }
+}
---
+++ IbgeClient/IbgeClient.Client/Extensions/EnumsExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+
+namespace IbgeClient.Client.Enums
+{
+    public static class EnumsExtensions
+    {
+        public static string GetEnumDescription(this Enum enumValue)
+        {
+            var enumField = enumValue.GetType().GetField(enumValue.ToString());
+
+            var descriptionAttr =
+                enumField.GetCustomAttributes(typeof(DescriptionAttribute), false)
+                .FirstOrDefault() as DescriptionAttribute;
+
+            return descriptionAttr?.Description ?? enumValue.ToString();
+        }
+
+        public static T GetEnumValueFromDescription<T>(string description)
+        {
+            var type = typeof(T);
+
+            var fields = type.GetFields();
+
+            var field = fields
+                .SelectMany(f =>
+                f.GetCustomAttributes(typeof(DescriptionAttribute), false),
+                (f, a) => new { Field = f, Att = a })
+                .SingleOrDefault(a => ((DescriptionAttribute)a.Att).Description == description);
+
+            return field == null ? default(T) : (T)field.Field.GetRawConstantValue();
+        }
+    }
+
+
+}
