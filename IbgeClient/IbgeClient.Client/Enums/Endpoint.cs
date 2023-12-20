a98ddac32ec2eb606289d6bacbbc8cb596d97f09
ezequiel-lima <joao.lima94@fatec.sp.gov.br>
2023-12-20 17:16:00 -0300

    CHORE: atualizado endpoint

--- IbgeClient/IbgeClient.Client/Enums/Endpoint.cs
+++ IbgeClient/IbgeClient.Client/Enums/Endpoint.cs
@@ -5,26 +5,26 @@ namespace IbgeClient.Client.Enums
     public enum Endpoint
     {
         #region Autenticação e Autorização
-        [Description("https://localhost:7122/v1/accounts")]
+        [Description("https://desafiobaltaibge.azurewebsites.net/v1/accounts")]
         Account,
-        [Description("https://localhost:7122/v1/login")]
+        [Description("https://desafiobaltaibge.azurewebsites.net/v1/login")]
         Login,
         #endregion
 
         #region Localidades
-        [Description("https://localhost:7122/v1/ibges")]
+        [Description("https://desafiobaltaibge.azurewebsites.net/v1/ibges")]
         GetIbges,
-        [Description("https://localhost:7122/v1/ibges")]
+        [Description("https://desafiobaltaibge.azurewebsites.net/v1/ibges")]
         PostIbges,
-        [Description("https://localhost:7122/v1/ibges")]
+        [Description("https://desafiobaltaibge.azurewebsites.net/v1/ibges")]
         PutIbges,
-        [Description("https://localhost:7122/v1/ibges/city")]
+        [Description("https://desafiobaltaibge.azurewebsites.net/v1/ibges/city")]
         GetIbgesCity,
-        [Description("https://localhost:7122/v1/ibges/state")]
+        [Description("https://desafiobaltaibge.azurewebsites.net/v1/ibges/state")]
         GetIbgesState,
-        [Description("https://localhost:7122/v1/ibges/codeIbge")]
+        [Description("https://desafiobaltaibge.azurewebsites.net/v1/ibges/codeIbge")]
         GetCodeIbges,
-        [Description("https://localhost:7122/v1/ibges/id")]
+        [Description("https://desafiobaltaibge.azurewebsites.net/v1/ibges/id")]
         DeleteIbges
         #endregion
     }
